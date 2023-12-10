2de2d957d35c67ec83441ac65ec7f95385548143
mish <moodles16@gmail.com>
2023-12-10 19:06:16 +1100

    remainder of JS code written, comments completed, readme completed.

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ A webpage that generates a random password from 8 - 128 characters according to 
 
 
 ### Tasks Completed
-- Funtions written to generate and return a randomized password on the screen in the appropriate box.
+- Functions and loops written to generate and return a randomized password on the screen in the appropriate box.
 - All possible characters entered as variables to allow for better randomization of returned password.
+- Code written in such a way that users will not be able to move forward until they select the correct amounts of characters and at least one character type.
+- The generated password will only contain the character types that the user has chosen.
 
 
 ### Links
@@ -17,22 +19,25 @@ A webpage that generates a random password from 8 - 128 characters according to 
 [Code Repository](https://github.com/alittlemishmash/Password-Generator.git)
 
 
-## Credits
-
-The user needs to click the "Generate Password" button to start the process of creating a random password.
-
 
 ## Usage
 
-This website can be used on any browser.
+- This website can be used on any browser.
+- The user needs to click the "Generate Password" button to start the process of creating a random password.
+- The user will need to follow a series of prompts in order to generate the password.
+- If the user enters the incorrect information according to the prompt, the user will need to stat back from the beginning of that particular loop before moving forward.
 
 ![Screenshot of index.html](./assets/website-screenshot.png)
+
 
 
 ## Credits
 
 - Monash BootCamp FullStack tutors and advisors.
 - https://www.programiz.com/javascript/examples/get-random-item#:~:text=random()%20method.,access%20a%20random%20array%20element.
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+- https://www.youtube.com/watch?v=gTdesbu8nyo
+- https://www.w3schools.com/js/js_loop_for.asp
 
 
 ## License
--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function generatePassword() {
 
   // Assignment of variables
-  var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
   var passLength;
+  var allChars = "";
+  var password = "";
 
+  // User to select the number range of 8-128 characters for password length. If nothing, or a non-numerical character is entered, then loop back to the choice prompt.
   do {
     passLength = prompt("Choose a password length of between 8 - 128 characters:");
     if (isNaN(passLength) || passLength < 8 || passLength > 128 || passLength === "") {
-      alert("Error! Please select a character length between 8 and 128.");}
+      alert("Error! Please select a character length between 8 and 128.");
+    }
   } while (isNaN(passLength) || passLength < 8 || passLength > 128 || passLength === "");
 
-  var chooselowerCase; 
+  // User to choose at least one character type, otherwise loop back to the beginning of the list.
+
+  var chooselowerCase;
   var chooseNumeric;
   var choosespecialChar;
   var chooseupperCase;
@@ -25,14 +31,28 @@ function generatePassword() {
     chooseNumeric = confirm("Do you want numbers?");
     choosespecialChar = confirm("Do you want special characters?");
     if (!(chooselowerCase || chooseupperCase || chooseNumeric || choosespecialChar)) {
-      alert("Error! Please select at least one character type!");}
+      alert("Error! Please select at least one character type!");
+    }
   } while (!(chooselowerCase || chooseupperCase || chooseNumeric || choosespecialChar));
 
+  // Concatenate character sets based on user choices
+  if (chooselowerCase) { allChars = allChars.concat(lowercaseChars); }
+  if (chooseupperCase) { allChars = allChars.concat(uppercaseChars); }
+  if (chooseNumeric) { allChars = allChars.concat(numericChars); }
+  if (choosespecialChar) { allChars = allChars.concat(specialChars); }
+
+
+  // Generate the password
+  for (let i = 0; i < passLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  // Return the generated password
+  return password;
 }
 
-
-
-
+//Show the password on the screen
 var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
